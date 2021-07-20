d14cfe80405894e4a77dd8c0e4d029a234b259d8
Seb <seb@neonjungle.studio>
2021-07-20 10:48:00 +1000

    inital commit

---
+++ animations.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function (e) {
+    const debug = true;
+    const scroller = scrollama();
+    const $sections = document.querySelectorAll(".l-prose-blocks__block");
+    scroller
+        .setup({
+            step: $sections,
+            offset: 0.3,
+            once: true,
+            debug: debug,
+        })
+        .onStepEnter(res => {
+            res.element.classList.add("is-visible");
+        })
+
+    window.addEventListener('resize', scroller.resize);
+});
